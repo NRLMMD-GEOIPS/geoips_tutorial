bd49f64c1810d9ef412ca7d23d9dfe146871710c
surratt <geoips@nrlmry.navy.mil>
2022-08-03 14:22:48 -0700

    Only use B14 for example scripts
    * goes16 static Infrared vs Visible
    * AMSR2 overlay on Infrared-Gray vs Visible
    NRLMMD-GEOIPS/geoips#25

--- example_scripts/config_script_amsr2_tc_windspeed.sh
+++ example_scripts/config_script_amsr2_tc_windspeed.sh
@@ -1,3 +1,3 @@
 run_procflow \
   --procflow config_based \
-  --output_config $GEOIPS_PACKAGES_DIR/geoips_tutorial/example_scripts/config_amsr2_tc_windspeed.yaml
+  --output_config $GEOIPS_PACKAGES_DIR/geoips_tutorial/example_scripts/output_config_amsr2_tc_windspeed.yaml
