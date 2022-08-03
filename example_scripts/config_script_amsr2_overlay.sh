87f63e5026276c83a2f1c6c777d0eb9ddc07d6fb
surratt <geoips@nrlmry.navy.mil>
2022-08-03 14:25:41 -0700

    Update CHANGELOG NRLMMD-GEOIPS/geoips#25

--- example_scripts/config_script_amsr2_overlay.sh
+++ example_scripts/config_script_amsr2_overlay.sh
@@ -3,5 +3,5 @@ run_procflow ${GEOIPS_TESTDATA_DIR}/test_data_amsr2/data/*mbt*.nc \
   --output_config ${GEOIPS_PACKAGES_DIR}/geoips_tutorial/example_scripts/output_config_amsr2_overlay.yaml \
   --fuse_files ${GEOIPS_TESTDATA_DIR}/test_data_amsr2/bg_data/ahi_20200518_0740/* \
   --fuse_reader ahi_hsd \
-  --fuse_product Visible \
+  --fuse_product Infrared-Gray \
   --fuse_resampled_read True
