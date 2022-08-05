9ce5230f2bfbcaa5cbdc72c047032ca2bbe81bc6
surratt <geoips@nrlmry.navy.mil>
2022-08-04 21:50:26 -0700

    Add mkdirs to file copy script
    NRLMMD-GEOIPS/geoips#12

--- plugin_example/extra_files/mv_files.sh
+++ plugin_example/extra_files/mv_files.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+if [ -e ../plugin_example ]; then
+    mv ../plugin_example ../my_plugin
+fi
+
+mkdir -p ../my_plugin/yaml_configs/product_params
+mkdir -p ../my_plugin/yaml_configs/product_inputs
+mkdir -p ../my_plugin/interface_modules/algorithms
+
 cp my_89pct.yaml ../my_plugin/yaml_configs/product_params/
 cp amsr2.yaml ../my_plugin/yaml_configs/product_inputs
 cp my_89pct_alg.py ../my_plugin/interface_modules/algorithms
