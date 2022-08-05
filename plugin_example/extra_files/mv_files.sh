403d74b10bf76d81c11f72592bd976ae4b85121d
surratt <geoips@nrlmry.navy.mil>
2022-08-04 21:19:31 -0700

    Add script to move files into place
    NRLMMD-GEOIPS/geoips#12

---
+++ plugin_example/extra_files/mv_files.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cp my_89pct.yaml ../my_plugin/yaml_configs/product_params/
+cp amsr2.yaml ../my_plugin/yaml_configs/product_inputs
+cp my_89pct_alg.py ../my_plugin/interface_modules/algorithms
