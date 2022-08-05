80c759b8ceb3a1e06e421d4aa236d98f0a380c92
surratt <geoips@nrlmry.navy.mil>
2022-08-04 21:36:55 -0700

    Adding complete setup.py, and test script
    NRLMMD-GEOIPS/geoips#12

--- plugin_example/extra_files/mv_files.sh
+++ plugin_example/extra_files/mv_files.sh
@@ -3,3 +3,4 @@
 cp my_89pct.yaml ../my_plugin/yaml_configs/product_params/
 cp amsr2.yaml ../my_plugin/yaml_configs/product_inputs
 cp my_89pct_alg.py ../my_plugin/interface_modules/algorithms
+cp setup.py ../
